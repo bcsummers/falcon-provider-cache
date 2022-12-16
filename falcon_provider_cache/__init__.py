789391198fc80f3f25bace3b7cca6e736a94ac50
Bracey Summers <bcsummers@gmail.com>
2022-12-16 10:12:16 -0600

    General cleanup - removed metadata file - updated pre-commit config - added codeql github action for security scanning

--- falcon_provider_cache/__init__.py
+++ falcon_provider_cache/__init__.py
@@ -1,12 +1,4 @@
 """Falcon cache module."""
 # flake8: noqa
-from .__metadata__ import (
-    __author__,
-    __author_email__,
-    __description__,
-    __license__,
-    __package_name__,
-    __url__,
-    __version__,
-)
-from .utils import RedisCacheProvider
+# first-party
+from falcon_provider_cache.utils import RedisCacheProvider
